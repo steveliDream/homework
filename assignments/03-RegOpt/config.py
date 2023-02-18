9c3fdd427ec8406abf5bf4f877691ac1c257f2d8
steveliDream <steveli.dream@gmail.com>
2023-02-18 14:13:55 -0500

    upload hw

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -8,7 +8,7 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 class CONFIG:
     batch_size = 64
     num_epochs = 10
-    initial_learning_rate = 0.005
+    initial_learning_rate = 0.004
     initial_weight_decay = 0
 
     lrs_kwargs = {
