29f11b8e758b53afcf244349f6ceba9982f6e660
Shuangshuang Li <shuangshuangli@kaikaixinxinMacBook-Pro.local>
2023-03-10 15:05:38 -0500

    upload hw

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -1,9 +1,33 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
 class Model(torch.nn.Module):
     def __init__(self, num_channels: int, num_classes: int) -> None:
-        ...
+        super(Model, self).__init__()
+        self.conv1 = nn.Conv2d(num_channels, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.fc1 = nn.Linear(12544, 128)
+        self.fc2 = nn.Linear(128, num_classes)
+        self.dropout1 = nn.Dropout(0.4)
+        self.dropout2 = nn.Dropout(0.2)
+        self.batch_norm1 = nn.BatchNorm2d(32)
+        self.batch_norm2 = nn.BatchNorm2d(64)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        ...
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.batch_norm1(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.batch_norm2(x)
+        x = F.max_pool2d(x, 2)
+        x = x.view(-1, 12544)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout1(x)
+        x = self.fc2(x)
+        x = self.dropout2(x)
+
+        return x
