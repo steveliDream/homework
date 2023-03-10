9a416c647a27d6e11c18a93da9c222fb7462c198
Shuangshuang Li <shuangshuangli@kaikaixinxinMacBook-Pro.local>
2023-03-10 16:03:25 -0500

    upload hw

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -12,32 +12,34 @@ class Model(torch.nn.Module):
         """
         as per requirement
         """
-        super(Model, self).__init__()
+        super(Model, self).__init__()      
         self.conv1 = nn.Conv2d(num_channels, 32, 3, 1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.fc1 = nn.Linear(12544, 128)
-        self.fc2 = nn.Linear(128, num_classes)
-        self.dropout1 = nn.Dropout(0.4)
-        self.dropout2 = nn.Dropout(0.2)
-        self.batch_norm1 = nn.BatchNorm2d(32)
-        self.batch_norm2 = nn.BatchNorm2d(64)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(2304, 256)
+        self.dropout = nn.Dropout(p=0.5)
+        self.fc2 = nn.Linear(256, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         as per requirement
         """
         x = self.conv1(x)
-        x = F.relu(x)
-        x = self.batch_norm1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)
         x = self.conv2(x)
-        x = F.relu(x)
-        x = self.batch_norm2(x)
-        x = F.max_pool2d(x, 2)
-        x = x.view(-1, 12544)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        x = self.pool2(x)
+        x = x.view(x.size(0), -1)
         x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout1(x)
+        x = self.dropout(x)
         x = self.fc2(x)
-        x = self.dropout2(x)
 
         return x
